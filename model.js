5a8eb0a9cda127c3867fd078e2fb0401b0354b31
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-24 18:06:44 +0200

    modified model.js added state variable to store weather data and created function to handle API call

--- model.js
+++ model.js
@@ -1,2 +1,21 @@
-import {API_KEY_OpenWeather,API_KEY_RestCountry} from '.key.js'
+import { API_KEY_OpenWeather, API_KEY_RestCountry } from "./config.js";
+import AJAX from "./helper.js";
 
+export const state = { weatherData: {} };
+
+function formatWeatherData(data) {
+  state.weatherData = {
+    name: data.name,
+    description: data.weather[0].description,
+    temp: data.main.temp,
+    country: data.sys.country,
+  };
+  console.log(state.weatherData);
+}
+
+export async function getAJAX() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=ravenna&units=metric&appid=${API_KEY_OpenWeather}`;
+  const data = await AJAX(url);
+  formatWeatherData(data)
+  return data;
+}
