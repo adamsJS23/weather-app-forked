5683e7493318dea4a74c005a871c7c67f4e0325e
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-24 16:04:15 +0200

    modified key.js

--- key.js
+++ key.js
@@ -1,4 +1,2 @@
-
-export default function useApiKey() {
-	return "47ffc4cddb8dd849937062b6a26d79f3";
-}
+export const API_KEY_OpenWeather='47ffc4cddb8dd849937062b6a26d79f3';
+export const API_KEY_RestCountry='';
