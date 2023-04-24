3fb325c50fd94bfba10218b052d92532621a5636
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-24 19:15:57 +0200

    added method to WeatherDataView class

--- weatherDataView.js
+++ weatherDataView.js
@@ -1,19 +1,39 @@
 class WeatherDataView {
   _data;
   _container = document.getElementById("weatherContainer");
+  _input = document.getElementById("locationInput");
 
   render() {
-    const html = `<div>
+    this._container.insertAdjacentHTML("afterbegin", this._generateMarkup());
+    this._clearInput();
+  }
+
+  _generateMarkup(_data) {
+    return `<div>
                     <p>
-                      <span style='font-size: 1.8rem;'>${this._data.name}, </span>
+                      <span style='font-size: 1.8rem;'>${
+                        this._data.name
+                      }, </span>
                       <br>
-                      <span style='font-size: 1.3rem;'> ${this._data.countryName}</span>
+                      <span style='font-size: 1.3rem;'> ${
+                        this._data.countryName
+                      }</span>
                     </p>
-                    <img src='https://openweathermap.org/img/wn/${icon}@2x.png'>
+                    <img src='https://openweathermap.org/img/wn/${
+                      this._data.icon
+                    }@2x.png'>
                   </div>
                   <div class="weather-details">
                     <p>${this._data.description}</p>
-                    <p style="font-size: 1.9rem">${Math.floor(this._data.temp)}°C</p>
+                    <p style="font-size: 1.9rem">${Math.floor(
+                      this._data.temp
+                    )}°C</p>
                   </div>`;
   }
+
+  _clearInput() {
+    this._input.value = "";
+  }
 }
+
+export default new WeatherDataView();
