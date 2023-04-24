917e6ce784d7680b6570f12d5756e033427d49f4
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-24 23:19:19 +0200

    refactoring

--- weatherDataView.js
+++ weatherDataView.js
@@ -1,14 +1,14 @@
 class WeatherDataView {
   _data;
-  _container = document.getElementById("weatherContainer");
-  _input = document.getElementById("locationInput");
+  _container = document.querySelector(".weatherContainer");
 
-  render() {
-    this._container.insertAdjacentHTML("afterbegin", this._generateMarkup());
-    this._clearInput();
+  render(data) {
+    this._data=data;
+    this._clearContainer()
+    this._container.insertAdjacentHTML("afterbegin", this._generateMarkup(this._data));
   }
 
-  _generateMarkup(_data) {
+  _generateMarkup(data) {
     return `<div>
                     <p>
                       <span style='font-size: 1.8rem;'>${
@@ -31,9 +31,11 @@ class WeatherDataView {
                   </div>`;
   }
 
-  _clearInput() {
-    this._input.value = "";
+  _clearContainer(){
+    this._container.innerHTML='';
   }
+
+  
 }
 
 export default new WeatherDataView();
