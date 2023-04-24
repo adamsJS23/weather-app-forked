e9a8ebdbebc860f5abdb841245b42248f74631f2
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-25 00:07:15 +0200

    added try catch block on helper.js

--- helper.js
+++ helper.js
@@ -1,8 +1,15 @@
 import { API_KEY_OpenWeather, API_KEY_RestCountry } from "./config.js";
 
 export default async function AJAX(url) {
-  const response = await fetch(url);
-  if (!response.ok) return;
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url);
+    if (!response.ok)
+      throw new Error("Invalid city name, please enter a valid city name");
+
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
 }
