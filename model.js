cfccee0f39aa596f578b639ae8875623714e07ea
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-24 18:45:19 +0200

    added some coments and also await data from OpenWeather and Restcountries

--- model.js
+++ model.js
@@ -1,21 +1,37 @@
-import { API_KEY_OpenWeather, API_KEY_RestCountry } from "./config.js";
+import {
+  API_KEY_OpenWeather,
+  API_KEY_RestCountry,
+  openWeatherUrl,
+  restCountryUrl,
+} from "./config.js";
 import AJAX from "./helper.js";
 
+// State variable
 export const state = { weatherData: {} };
 
-function formatWeatherData(data) {
+// Formating Data recieved from OpenWeather
+function formatWeatherData(data, country) {
   state.weatherData = {
     name: data.name,
     description: data.weather[0].description,
     temp: data.main.temp,
-    country: data.sys.country,
+    countryName: country,
   };
   console.log(state.weatherData);
 }
 
+// Await data form OpenWeather API and RestCountries API
 export async function getAJAX() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=ravenna&units=metric&appid=${API_KEY_OpenWeather}`;
-  const data = await AJAX(url);
-  formatWeatherData(data)
-  return data;
+  const weatherData = await AJAX(
+    `${openWeatherUrl}weather?q=ravenna&units=metric&appid=${API_KEY_OpenWeather}`
+  );
+
+  const countryData = await AJAX(
+    `${restCountryUrl}${weatherData.sys.country.toLowerCase()}`
+  );
+  const country = countryData[0].name.common;
+
+  formatWeatherData(weatherData, country);
+
+  return weatherData;
 }
