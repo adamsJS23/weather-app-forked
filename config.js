4bfcd06203bc6575a6bafcbc6c4b20e74ec7a90f
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-24 19:02:07 +0200

    added openWeatherUrl and restCountryUrl

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 export const API_KEY_OpenWeather='47ffc4cddb8dd849937062b6a26d79f3';
 export const API_KEY_RestCountry='';
+export const openWeatherUrl='https://api.openweathermap.org/data/2.5/'
+export const restCountryUrl='https://restcountries.com/v3.1/alpha/'
